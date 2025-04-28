3f396411719e0d630a718417385b708b97d2abde
public-release <as-public-release@janestreet.com>
2025-04-28 19:35:04 +0100

    v0.18~preview.130.26+1192

--- am_running_how_js.mli
+++ am_running_how_js.mli
@@ -7,12 +7,12 @@ open! Core
     - [`Node_benchmark] means that the code is being run using node as part of a benchmark
     - [`Node] means that the code is being run using node, but not as part of an
       expect_test or a benchmark
-    - [`Browser_test] means that the code is being run in a browser as part of an expect_test
+    - [`Browser_test] means that the code is being run in a browser as part of an
+      expect_test
     - [`Browser_benchmark] means that the code is being run in the browser as part of a
       benchmark
     - [`Browser] means that the code is being run in a browser but not as part of a
-      benchmark
-*)
+      benchmark *)
 val am_running_how
   : [ `Browser
     | `Browser_test
@@ -23,6 +23,9 @@ val am_running_how
     | `Node_jsdom_test
     ]
 
-(** Returns true iff [am_running_how] is [`Browser], [`Browser_benchmark],
-    or [`Browser_test]. *)
+(** Returns true iff [am_running_how] is [`Browser], [`Browser_benchmark], or
+    [`Browser_test]. *)
 val am_in_browser : bool
+
+(** Like [am_in_browser], but also returns [true] if [`Node_jsdom_test]. *)
+val am_in_browser_like_api : bool
